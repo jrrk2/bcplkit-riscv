8fb0af776f079f21f7392393688124e5ed24bdf9
Jonathan Kimmitt <jonathan@kimmitt.uk>
2025-01-09 09:04:50 +0000

    Use ICINT register names in disassembly

--- src/trial.sh
+++ src/trial.sh
@@ -7,12 +7,14 @@ cat iclib.i blib.i xg.i >xg0.int
 ./icint cg1.int < OCODE
 cat iclib.i blib.i INTCODE >$1.int
 mv INTCODE $1.i
+./icint -l $1.int
+mv icint.log $1.iclog
 ./icint xg0.int < $1.int
 mv ASM $1.s
 sed -e 's=a5=M=g' -e 's=s4=G=g' -e 's=s5=P=g' -e 's=s6=A=g' -e 's=s7=B=g' -e 's=s9=D=g' $1.s > $1.asm
-gcc -g -o $1 -static blib.c $1.s
-objdump -d $1 | sed -e 's=a5=M=g' -e 's=s4=G=g' -e 's=s5=P=g' -e 's=s6=A=g' -e 's=s7=B=g' -e 's=s9=D=g' > $1.dis
+gcc -g -o $1.exe -static blib.c $1.s
+objdump -d $1.exe | sed -e 's=a5=M=g' -e 's=s4=G=g' -e 's=s5=P=g' -e 's=s6=A=g' -e 's=s7=B=g' -e 's=s9=D=g' > $1.dis
 rm -f logfile.txt
-gdb --batch-silent ./$1 -x commands.gdb 
+gdb --batch-silent ./$1.exe -x commands.gdb 
 sed -e 's=a5=M=g' -e 's=s4=G=g' -e 's=s5=P=g' -e 's=s6=A=g' -e 's=s7=B=g' -e 's=s9=D=g' logfile.txt > $1.log
 rm -f logfile.txt
